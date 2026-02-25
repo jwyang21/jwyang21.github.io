68ec5d7a72fc2d8e8d62177a7d61acff1b6b6bab
jwyang21 <jwyang.ai@gmail.com>
2026-02-25 11:47:23 +0000

    Deploying to gh-pages from @ jwyang21/jwyang21.github.io@c071dc256097a862508bfebc14ff1c00a29c8ee7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,12 +429,12 @@ ninja.data = [{
               window.location.href = "/teachings/2022_algorithm/";
             },},{id: "teachings-machine-learning-for-healthcare",
           title: 'Machine Learning for Healthcare',
-          description: "",
+          description: "Teaching Assistant (TA)",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/2025_ml4h/";
             },},{id: "teachings-programming-for-ai",
           title: 'Programming for AI',
-          description: "",
+          description: "Teaching Assistant (TA)",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/2025_pr4ai/";
             },},{
